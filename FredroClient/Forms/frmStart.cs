5e37a4ee3857c27c4f5976e38ea17fa3eda82b54
Fredro Rourke <@>
2018-10-24 16:31:03 +0300

    commit

--- FredroClient/Forms/frmStart.cs
+++ FredroClient/Forms/frmStart.cs
@@ -14,6 +14,8 @@ namespace FredroClient.Forms
 {
     public sealed partial class frmStart : FredroBaseXtraForm
     {
+        private readonly CredentialModel _model;
+
         public frmStart()
         {
             InitializeComponent();
@@ -22,7 +24,7 @@ namespace FredroClient.Forms
 
         private void InitControls()
         {
-            //sleHostname.DataBindings.Add();
+            sleHostname.DataBindings.Add(new Binding("EditValue", DataSource, property.Name, true, DataSourceUpdateMode.OnPropertyChanged));
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -34,5 +36,28 @@ namespace FredroClient.Forms
         {
 
         }
+
+        private sealed class CredentialModel
+        {
+            public enum EmailServer
+            {
+                [Description("Gmail сервер. Пример: example@gmail.com")]
+                gmail,
+                [Description("Mail сервер. Пример: example@mail.ru")]
+                mail,
+                [Description("Yandex сервер. Пример: example@yandex.ru")]
+                yandex
+            }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public EmailServer CurrentServer { get; set; }
+
+        }
+
+        private sealed class EmailServer
+        {
+
+        }
+
     }
 }
