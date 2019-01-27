7f5afc79bc7da0b7df9c9924eacc2dad9a704f87
Пользователь <@EF-01121994>
2019-01-27 19:55:24 +0300

    added to client new service reference

--- FredroDAL/Models/DatabaseObjectModels/DbObjectBaseModel.cs
+++ FredroDAL/Models/DatabaseObjectModels/DbObjectBaseModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace FredroDAL.Models.DatabaseObjectModels
 {
+    [DataContract]
     public class DbObjectBaseModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
