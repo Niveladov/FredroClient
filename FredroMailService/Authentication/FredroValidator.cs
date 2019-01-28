4f7c5fa7d7794d977aeb4bc22fa047c6553a9508
Fredro Rourke <@>
2019-01-28 17:37:57 +0300

    added custom authentication configs to host project

---
+++ FredroMailService/Authentication/FredroValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FredroMailService.Authentication
+{
+    internal class FredroValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (!string.IsNullOrWhiteSpace(userName) || !string.IsNullOrWhiteSpace(password)) return;
+            else throw new SecurityTokenException("Account is invalid");
+        }
+    }
+}
