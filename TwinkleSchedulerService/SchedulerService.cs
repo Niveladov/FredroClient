ca4a6351eda0b219943ae8310cc1cd008253ae18
Fredro Rourke <@>
2019-03-13 19:28:32 +0300

    added scheduler service project

---
+++ TwinkleSchedulerService/SchedulerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using FredroDAL.Models.DatabaseObjectModels.Views;
+
+namespace TwinkleSchedulerService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class SchedulerService : ISchedulerService
+    {
+        public IEnumerable<ViewVehicle> GetResources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Join()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AssignAppointment(int appointmentId, int resourceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelAppointment(int appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAppointment(int appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
