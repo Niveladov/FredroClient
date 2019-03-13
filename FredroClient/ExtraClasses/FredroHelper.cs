f4900a4464256f5864301ce20fac355b4cad823a
Fredro Rourke <@>
2019-03-13 18:31:45 +0300

    little commit

--- FredroClient/ExtraClasses/FredroHelper.cs
+++ FredroClient/ExtraClasses/FredroHelper.cs
@@ -22,123 +22,27 @@ namespace FredroClient.ExtraClasses
 {
     internal static class FredroHelper
     {
-        //const string PLAIN_TEXT = "text/plain";
-        //const string HTML_TEXT = "text/html";
-
-        //public static string GetDescription(this Enum enumerationValue)
+        //internal static async void SaveNewMessages(List<TheMail> allMails)
         //{
-        //    //Tries to find a DescriptionAttribute for a potential friendly name
-        //    //for the enum
-        //    MemberInfo[] memberInfo = enumerationValue.GetType().GetMember(enumerationValue.ToString());
-        //    if (memberInfo != null && memberInfo.Length > 0)
+        //    using (var db = new FredroDbContext())
         //    {
-        //        object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-        //        if (attrs != null && attrs.Length > 0)
+        //        foreach (var message in allMails)
         //        {
-        //            //Pull out the description value
-        //            return ((DescriptionAttribute)attrs[0]).Description;
+        //            db.Mails.AddIfNotExists(message);
         //        }
-        //    }
-        //    //If we have no description attribute, just return the ToString of the enum
-        //    return enumerationValue.ToString();
-        //}
-
-        //internal static List<Message> FetchAllMessages(string hostname, int port, bool useSsl, string username, string password)
-        //{
-        //    try
-        //    {
-        //        // The client disconnects from the server when being disposed
-        //        using (var client = new Pop3Client())
-        //        {
-        //            // Connect to the server
-        //            client.Connect(hostname, port, useSsl);
-        //            // Authenticate ourselves towards the server
-        //            client.Authenticate(username, password);
-        //            // Get the number of messages in the inbox
-        //            var messageCount = client.GetMessageCount();
-        //            // We want to download all messages
-        //            var allMessages = new List<Message>(messageCount);
-        //            // Messages are numbered in the interval: [1, messageCount]
-        //            // Ergo: message numbers are 1-based.
-        //            // Most servers give the latest message the highest number
-        //            for (int i = messageCount; i > 0; i--)
-        //            {
-        //                allMessages.Add(client.GetMessage(i));
-        //            }
-        //            // Now return the fetched messages
-        //            return allMessages;
-        //        }
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        FredroMessageBox.ShowError(ex.Message);
-        //        return null;
+        //        await db.SaveChangesAsync();
         //    }
         //}
 
-        //internal static TheMail GetTheMail(this Message message)
+        //internal static void TruncateMails()
         //{
-        //    var attachmentParts = message.FindAllAttachments();
-        //    var plainTextParts = message.FindAllMessagePartsWithMediaType(PLAIN_TEXT);
-        //    var htmlTextParts = message.FindAllMessagePartsWithMediaType(HTML_TEXT);
-        //    var theMail = new TheMail();
-        //    theMail.Id = message.Headers.MessageId;
-        //    theMail.FromFullRaw = message.Headers.From.Raw;
-        //    theMail.FromAddress = message.Headers.From.Address;
-        //    theMail.FromDisplayName = message.Headers.From.DisplayName;
-        //    theMail.ToFullRaw = message.Headers.To.FirstOrDefault()?.Raw;
-        //    theMail.ToAddress = message.Headers.To.FirstOrDefault()?.Address;
-        //    theMail.ToDisplayName = message.Headers.To.FirstOrDefault()?.DisplayName;
-        //    theMail.Date = message.Headers.DateSent;
-        //    theMail.Subject = message.Headers.Subject;
-        //    theMail.Body = plainTextParts.FirstOrDefault()?.GetBodyAsText() ??
-        //                        htmlTextParts.FirstOrDefault()?.GetBodyAsText();
-        //    //---↓↓↓---костыль---↓↓↓---
-        //    if (theMail.FromDisplayName.Contains("??") || theMail.Subject.Contains("??"))
+        //    using (var db = new FredroDbContext())
         //    {
-        //        var messageBytes = message.RawMessage;
-        //        var messageString = Encoding.UTF8.GetString(messageBytes);
-        //        if (theMail.FromDisplayName.Contains("??"))
-        //        {
-        //            var regexFrom = new Regex("From:\\s(.*)\r\n");
-        //            var matchFrom = regexFrom.Match(messageString);
-        //            var fullFromString = matchFrom.Value.Trim().Substring(6).Replace("\"", ""); //6 = "From: ".length
-        //            theMail.FromFullRaw = fullFromString;
-        //            theMail.FromDisplayName = fullFromString.Substring(0, fullFromString.IndexOf('<') - 1);
-        //        }
-        //        if (theMail.Subject.Contains("??"))
-        //        {
-        //            var regexSubj = new Regex("Subject:\\s(.*)\r\n");
-        //            var matchSubj = regexSubj.Match(messageString);
-        //            var fullSubjString = matchSubj.Value.Trim().Substring(9); // = "Subject: ".length
-        //            theMail.Subject = fullSubjString;
-        //        }
+        //        db.Database.ExecuteSqlCommand("TRUNCATE TABLE TheMails");
         //    }
-        //    //---↑↑↑---костыль---↑↑↑---
-        //    return theMail;
         //}
 
-        internal static async void SaveNewMessages(List<TheMail> allMails)
-        {
-            using (var db = new FredroDbContext())
-            {
-                foreach (var message in allMails)
-                {
-                    db.Mails.AddIfNotExists(message);
-                }
-                await db.SaveChangesAsync();
-            }
-        }
-
-        internal static void TruncateMails()
-        {
-            using (var db = new FredroDbContext())
-            {
-                db.Database.ExecuteSqlCommand("TRUNCATE TABLE TheMails");
-            }
-        }
-
+        //resource 1 time
         internal static BindingList<ViewVehicle> GetAllViewVehicles()
         {
             try
@@ -198,7 +102,7 @@ namespace FredroClient.ExtraClasses
                 throw ex;
             }
         }
-
+        //appts many times
         internal static BindingList<ViewAssignedDeal> GetAllViewAssignedDeals()
         {
             try
@@ -218,7 +122,7 @@ namespace FredroClient.ExtraClasses
                 throw ex;
             }
         }
-
+        //
         internal static Deal GetDeal(int dealId)
         {
             try
@@ -276,7 +180,7 @@ namespace FredroClient.ExtraClasses
                 throw ex;
             }
         }
-
+        //will be appts many times
         internal static List<Deal> GetNotAssignedDeals()
         {
             try
