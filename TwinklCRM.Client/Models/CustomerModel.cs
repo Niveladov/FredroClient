5af1c40eaef226a8d691ca4b4fdc849e2c4a22f0
Fredro Rourke <www.hello@net.com>
2019-04-17 18:27:48 +0300

    saving new customers

--- TwinklCRM.Client/Models/CustomerModel.cs
+++ TwinklCRM.Client/Models/CustomerModel.cs
@@ -24,7 +24,7 @@ namespace TwinklCRM.Client.Models
         {
             try
             {
-                //_boServiceClient.InsertPerformer
+                _boServiceClient.InsertCustomer(CurrentCustomer);
             }
             catch
             {
