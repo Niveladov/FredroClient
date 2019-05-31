246ba2ee80c731c78aab3c4f0f56883c9cf95f0f
Fredro Rourke <www.hello@net.com>
2019-05-31 17:17:45 +0300

    +

--- TwinklCRM.BusinessObjectServiceLibrary/BusinessObjectService.cs
+++ TwinklCRM.BusinessObjectServiceLibrary/BusinessObjectService.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using TwinklCRM.BusinessObjectServiceLibrary.Models;
+using TwinklCRM.BusinessObjectServiceLibrary.Models.Interfaces;
 using TwinklCRM.DAL.Models.DatabaseObjectModels.Tables;
 using TwinklCRM.DAL.Models.DatabaseObjectModels.Tables.Dictionaries;
 
